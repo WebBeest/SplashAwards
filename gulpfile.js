122af68b4bcbc390d796d809de6ebb1d5b5925ee
Patrick Kannekens <patrick@web-beest.nl>
2024-05-08 10:57:43 +0200

    feat(storybook): Story - Link list - Add story, SDC and styling for link lists.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 'use strict';
-var gulp = require('gulp');
-var sass = require('gulp-sass')(require('node-sass'));
-var concat = require('gulp-concat');
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('node-sass'));
+const sourceMaps = require('gulp-sourcemaps');
+
 sass.compiler = require('node-sass');
 gulp.task('sass', function () {
   return gulp.src('./web/themes/custom/**/*.scss')
+    .pipe(sourceMaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(sourceMaps.write())
     .pipe(gulp.dest('./web/themes/custom/'));
 });
